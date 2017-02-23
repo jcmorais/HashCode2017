a6c98c9d47e94c1cfeae28ab10308a6ba188544f
Carlos Morais <jcmorais93@gmail.com>
2017-02-23 17:31:11 +0000

    first commit

---
+++ src/main/java/Run.java
@@ -0,0 +1,9 @@
+/**
+ * Created by carlosmorais on 23/02/2017.
+ */
+public class Run {
+    public static void main(String[] args) {
+        System.out.println("Hello HashCode 2017 !!!");
+    }
+}
+
